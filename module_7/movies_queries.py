679090676e09ccf7f055a5b4aa24f555cc849b62
Abe <acabanrios@my365.bellevue.edu>
2022-11-20 00:37:46 -0500

    11/19/2022 commit6

--- module_7/movies_queries.py
+++ module_7/movies_queries.py
@@ -1,1 +1,45 @@
-print("hello")
+
+import mysql.connector
+from mysql.connector import errorcode
+# config
+db = mysql.connector.connect(
+        user="abe",
+        password="nerds",
+        host="127.0.0.1",
+        database="movies",
+        # "raise_on_warnings": True
+        )
+
+# try:
+# db = (**config)
+
+#     print("\n Database user {} connected to MySQL on host {} with database {}"
+#           .format(config["user"], config["host"], config["database"]))
+#
+#     input("\n\n Press any key to continue...")
+#
+#     if db.is_connected():
+#         print("\n Connection established...")
+#
+# except mysql.connector.Error as err:
+#     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+#         print("   The supplied username or password are invalid")
+#
+#     elif err.errno == errorcode.ER_BAD_DB_ERROR:
+#         print("  The specified database does not exist")
+#
+#     else:
+#         print(err)
+#
+# finally:
+
+cursor = db.cursor()
+query = "SELECT studio_id, studio_name FROM studio;"
+cursor.execute(query)  # selecting three fields
+studios = cursor.fetchall()
+print("-- DISPLAYING Studio RECORDS --")
+for studio in studios:
+    print("Studio ID:{} \nStudio Name:{} \n".format(studio[0], studio[1]))  # three fields
+    # print(x)
+# print("Studio ID: {} \n Studio Name:{} \n Email:{} \n", format(studio[0], studio[1], studio[2]))
+db.close()
