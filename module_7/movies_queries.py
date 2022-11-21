56fc640b8a49495d4f697e2fbadc984e945dcc83
Abe <acabanrios@my365.bellevue.edu>
2022-11-21 14:29:54 -0500

    11/20/2022 commit7

--- module_7/movies_queries.py
+++ module_7/movies_queries.py
@@ -10,30 +10,30 @@ db = mysql.connector.connect(
 
 
 cursor = db.cursor()
-# query = "SELECT studio_id, studio_name FROM studio;"
-# cursor.execute(query)
-# studios = cursor.fetchall()
-# print("\n")
-# print("-- DISPLAYING Studio RECORDS --")
-# for studio in studios:
-#     print("Studio ID:{} \nStudio Name:{} \n".format(studio[0], studio[1]))
-#
-#
-# query = "SELECT genre_id, genre_name FROM genre;"
-# cursor.execute(query)
-# genres = cursor.fetchall()
-# print("\n")
-# print("-- DISPLAYING Genre RECORDS --")
-# for genre in genres:
-#     print("Genre Id:{} \nGenre Name:{} \n".format(genre[0], genre[1]))
-#
-# query = "SELECT film_name, film_runtime FROM film WHERE film_runtime <= 120;"
-# cursor.execute(query)
-# films = cursor.fetchall()
-# print("\n")
-# print("-- DISPLAYING Short Film RECORDS --")
-# for film in films:
-#     print("Film Name:{} \nRuntime:{} \n".format(film[0], film[1]))
+query = "SELECT studio_id, studio_name FROM studio;"
+cursor.execute(query)
+studios = cursor.fetchall()
+print("\n")
+print("-- DISPLAYING Studio RECORDS --")
+for studio in studios:
+    print("Studio ID:{} \nStudio Name:{} \n".format(studio[0], studio[1]))
+
+
+query = "SELECT genre_id, genre_name FROM genre;"
+cursor.execute(query)
+genres = cursor.fetchall()
+print("\n")
+print("-- DISPLAYING Genre RECORDS --")
+for genre in genres:
+    print("Genre Id:{} \nGenre Name:{} \n".format(genre[0], genre[1]))
+
+query = "SELECT film_name, film_runtime FROM film WHERE film_runtime <= 120;"
+cursor.execute(query)
+films = cursor.fetchall()
+print("\n")
+print("-- DISPLAYING Short Film RECORDS --")
+for film in films:
+    print("Film Name:{} \nRuntime:{} \n".format(film[0], film[1]))
 
 query = "SELECT film_name, film_director FROM film GROUP BY film_name, film_director ORDER BY film_director;"
 cursor.execute(query)
