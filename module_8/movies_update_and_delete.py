40015704077df5cdf483c1858b7e75550a57e46e
Abe <acabanrios@my365.bellevue.edu>
2022-12-02 18:10:18 -0500

    12/2/2022 commit

--- module_8/movies_update_and_delete.py
+++ module_8/movies_update_and_delete.py
@@ -25,45 +25,13 @@ cursor.execute("INSERT INTO studio (studio_id, studio_name) values(4, 'Pinewood 
 cursor.execute("""INSERT INTO film (film_id, film_name, film_releaseDate, film_runtime, film_director, genre_id, studio_id) values(4, 'The Fifth Element', '1997', '126', 'Luc Besson', 2, 4);
 """)
 
-def show_films(cursor, title):
-
-        cursor.execute("""SELECT film_name as Name, film_director as Director, genre_name as Genre, studio_name as 'Studio Name' 
-                        from film INNER JOIN genre ON film.genre_id=genre.genre_id INNER JOIN studio ON film.studio_id = studio.studio_id""")
-
-        films = cursor.fetchall()
-        print("\n -- {} --".format(title))
-
-        for film in films:
-            print("Film Name: {}\nDirector: {}\nGenre Name ID: {}\nStudio Name: {}\n".format(film[0], film[1], film[2], film[3]))
-
 show_films(cursor, "Displaying Film After Insert")
 
 cursor.execute("""UPDATE film SET film_id = 2, film_name = 'Alien', film_releaseDate = '1979', film_runtime = '117', film_director = 'Ridley Scott', studio_id = 1, genre_id = 1 WHERE film_id = 2;""")
-
-def show_films(cursor, title):
-
-        cursor.execute("""SELECT film_name as Name, film_director as Director, genre_name as Genre, studio_name as 'Studio Name' 
-                        from film INNER JOIN genre ON film.genre_id=genre.genre_id INNER JOIN studio ON film.studio_id = studio.studio_id""")
-
-        films = cursor.fetchall()
-        print("\n -- {} --".format(title))
-
-        for film in films:
-            print("Film Name: {}\nDirector: {}\nGenre Name ID: {}\nStudio Name: {}\n".format(film[0], film[1], film[2], film[3]))
 
 show_films(cursor, "Displaying Film After Update- Changed Alien To Horror --")
 
 cursor.execute("DELETE FROM film WHERE film_id = 1;")
 
-def show_films(cursor, title):
-
-        cursor.execute("""SELECT film_name as Name, film_director as Director, genre_name as Genre, studio_name as 'Studio Name' 
-                        from film INNER JOIN genre ON film.genre_id=genre.genre_id INNER JOIN studio ON film.studio_id = studio.studio_id""")
-
-        films = cursor.fetchall()
-        print("\n -- {} --".format(title))
-
-        for film in films:
-            print("Film Name: {}\nDirector: {}\nGenre Name ID: {}\nStudio Name: {}\n".format(film[0], film[1], film[2], film[3]))
 
 show_films(cursor, "Displaying Film After Delete")
