b37f755cc99d89b0698152305fb98a01e72e3ebb
Abe <acabanrios@my365.bellevue.edu>
2022-12-02 17:19:08 -0500

    12/2/2022 commit

--- module_8/movies_update_and_delete.py
+++ module_8/movies_update_and_delete.py
@@ -21,6 +21,10 @@ def show_films(cursor, title):
 
 show_films(cursor, "Displaying Films")
 
+cursor.execute("INSERT INTO studio (studio_id, studio_name) values(4, 'Pinewood Studios');")
+cursor.execute("""INSERT INTO film (film_id, film_name, film_releaseDate, film_runtime, film_director, genre_id, studio_id) values(4, 'The Fifth Element', '1997', '126', 'Luc Besson', 2, 4);
+""")
+
 def show_films(cursor, title):
 
         cursor.execute("""SELECT film_name as Name, film_director as Director, genre_name as Genre, studio_name as 'Studio Name' 
@@ -34,6 +38,8 @@ def show_films(cursor, title):
 
 show_films(cursor, "Displaying Film After Insert")
 
+cursor.execute("""UPDATE film SET film_id = 2, film_name = 'Alien', film_releaseDate = '1979', film_runtime = '117', film_director = 'Ridley Scott', studio_id = 1, genre_id = 1 WHERE film_id = 2;""")
+
 def show_films(cursor, title):
 
         cursor.execute("""SELECT film_name as Name, film_director as Director, genre_name as Genre, studio_name as 'Studio Name' 
@@ -46,6 +52,8 @@ def show_films(cursor, title):
             print("Film Name: {}\nDirector: {}\nGenre Name ID: {}\nStudio Name: {}\n".format(film[0], film[1], film[2], film[3]))
 
 show_films(cursor, "Displaying Film After Update- Changed Alien To Horror --")
+
+cursor.execute("DELETE FROM film WHERE film_id = 1;")
 
 def show_films(cursor, title):
 
